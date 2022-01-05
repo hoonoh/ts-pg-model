8ce334d250c9d6410ad7587db740f60715761b52
hoonoh <hoonoh@users.noreply.github.com>
2022-01-05 13:07:05 +0900

    chore: drop conposite types test cases
    composite types are not wildly used in js world since `pg` does
    not support it fully yet, and is blocking type mapping dev.
    https://github.com/brianc/node-postgres/issues/1801

--- src/test/migrations/1638256764713_init.ts
+++ src/test/migrations/1638256764713_init.ts
@@ -6,15 +6,6 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.addExtension('uuid-ossp', { ifNotExists: true });
 
-  // public
-  pgm.createType(
-    { schema: 'public', name: 'domain' },
-    {
-      host: 'text',
-      version: 'int2',
-    },
-  );
-
   // users
   pgm.createSchema('users');
 
@@ -43,33 +34,16 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     'android',
     'version',
   ]);
-  pgm.createType(
-    { schema: 'users', name: 'user_agent_version' },
-    {
-      family: 'text',
-      major: 'int2',
-      minor: 'int2',
-      patch: 'int2',
-    },
-  );
-  pgm.createType(
-    { schema: 'users', name: 'user_agent' },
-    {
-      source: 'text',
-      browser: 'users.user_agent_browser',
-      agent: 'users.user_agent_version',
-      device: 'users.user_agent_version',
-      operating_system: 'users.user_agent_version',
-    },
-  );
   pgm.createTable(
     { schema: 'users', name: 'sessions' },
     {
-      domain: { type: 'public.domain', notNull: true },
       user_id: { references: { schema: 'users', name: 'users' }, type: 'int' },
       token: { type: 'text', notNull: true },
       ip: { type: 'inet', notNull: true },
-      user_agent: { type: 'users.user_agent', notNull: true },
+      user_agent: { type: 'text', notNull: true },
+      browser: { type: 'users.user_agent_browser', notNull: true },
+      device: { type: 'text', notNull: true },
+      operating_system: { type: 'text', notNull: true },
       created_at: { type: 'timestamptz', notNull: true, default: pgm.func('CURRENT_TIMESTAMP') },
       updated_at: { type: 'timestamptz', notNull: true, default: pgm.func('CURRENT_TIMESTAMP') },
     },
@@ -82,17 +56,15 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 
   pgm.createType({ schema: 'media', name: 'image_type' }, ['jpg', 'gif', 'webp']);
 
-  pgm.createType({ schema: 'media', name: 'dimension' }, { width: 'int', height: 'int' });
-
   pgm.createTable(
     { schema: 'media', name: 'images' },
     {
-      domain: { type: 'public.domain', notNull: true },
       id: { type: 'uuid', primaryKey: true, default: pgm.func('uuid_generate_v4()') },
       type: { type: 'media.image_type', notNull: true },
       filename: { type: 'text', notNull: true },
       filesize: { type: 'int', notNull: true },
-      dimension: { type: 'media.dimension', notNull: true },
+      width: { type: 'int2', notNull: true },
+      height: { type: 'int2', notNull: true },
       owner: { type: 'serial', references: { schema: 'users', name: 'users' } },
       created_at: { type: 'timestamptz', notNull: true, default: pgm.func('CURRENT_TIMESTAMP') },
       updated_at: { type: 'timestamptz', notNull: true, default: pgm.func('CURRENT_TIMESTAMP') },
@@ -104,15 +76,10 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.dropTable({ schema: 'media', name: 'images' });
   pgm.dropType({ schema: 'media', name: 'image_type' });
-  pgm.dropType({ schema: 'media', name: 'dimension' });
   pgm.dropSchema('media');
 
   pgm.dropTable({ schema: 'users', name: 'sessions' });
-  pgm.dropType({ schema: 'users', name: 'user_agent' });
   pgm.dropType({ schema: 'users', name: 'user_agent_browser' });
-  pgm.dropType({ schema: 'users', name: 'user_agent_version' });
   pgm.dropTable({ schema: 'users', name: 'users' });
   pgm.dropSchema('users');
-
-  pgm.dropType({ schema: 'public', name: 'domain' });
 }
