80ae573b5602c3dbdd77091045c56fce5ec53f87
hoonoh <hoonoh@users.noreply.github.com>
2022-01-24 17:25:00 +0900

    chore: semantic-release setup update

---
+++ .releaserc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  branches: ['main', { name: 'preview', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle: '# ts-pg-model changelog',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
